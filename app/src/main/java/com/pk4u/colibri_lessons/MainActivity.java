21201fb0c04c4e38c8eec2a7f8c4dbdfb52f382d
PK4U_S <Stepanchuk.ei@gmail.com>
2021-07-15 17:09:52 +0300

    Библиотеки для картинки

--- app/src/main/java/com/pk4u/colibri_lessons/MainActivity.java
+++ app/src/main/java/com/pk4u/colibri_lessons/MainActivity.java
@@ -3,12 +3,21 @@ package com.pk4u.colibri_lessons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView userImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userImageView = findViewById(R.id.UserImageView);
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(userImageView);
     }
 }
