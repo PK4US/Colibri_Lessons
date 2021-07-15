ae55a26138a239f440fdad59d46e42b83ce34c56
PK4U_S <Stepanchuk.ei@gmail.com>
2021-07-15 18:16:33 +0300

    Экран пользователей и поиска

--- app/src/main/java/com/pk4u/colibri_lessons/MainActivity.java
+++ app/src/main/java/com/pk4u/colibri_lessons/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userImageView = findViewById(R.id.UserImageView);
+        userImageView = findViewById(R.id.userImageView);
         Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(userImageView);
     }
 }
